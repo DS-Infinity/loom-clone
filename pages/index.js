54c265d0c01b27b135f4f3c72708ff96fd278bea
Daksh <dakshshrivastava2007@gmail.com>
2025-09-30 23:29:45 +0530

    works with some bugs

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import { Geist, Geist_Mono } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
 import { useState } from 'react';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export default function Home() {
   const [navOpen, setNavOpen] = useState(false);
